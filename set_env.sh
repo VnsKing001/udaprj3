861ad44de9cd58d16c0b57f2c60af9348e62cd4a
vnskingmotor <57930370+VnsKing001@users.noreply.github.com>
2022-06-23 20:17:41 +0700

    git commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=VnsKing7899
+export POSTGRES_HOST=database-1.csnltkjxydxi.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=mydb
+export AWS_BUCKET=elasticbeanstalk-us-east-1-643327991368
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
---
+++ udagram-api-feed/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': "postgres",
+  'password': "VnsKing7899",
+  'database': "mydb",
+  'host': "database-1.csnltkjxydxi.us-east-1.rds.amazonaws.com",
+  'dialect': 'postgres',
+  'aws_region': "us-east-1",
+  'aws_profile': "default",
+  'aws_media_bucket': "elasticbeanstalk-us-east-1-643327991368",
+  'url': "http://localhost:8100",
+  'jwt': {
+    'secret': "testing",
+  },
+};
